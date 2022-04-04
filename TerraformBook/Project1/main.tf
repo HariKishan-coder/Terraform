0dbc0b6f57ad3c98c62a2608ad49bd83b4f733cf
hpunati <hari.kishan.edu@oracle.com>
2022-04-04 17:54:11 +1000

    corrected files

--- TerraformBook/Project1/main.tf
+++ TerraformBook/Project1/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0916f5ee07e7b15d6"
   instance_type = "t2.micro"
 
   tags = {
